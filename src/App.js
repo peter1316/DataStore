2983c25f93d857e8a4c7bcc05acc117a4cd73e26
Peter Huang <peter.huang@belkin.com>
2023-09-15 14:58:55 +0800

    run ''amplify codegen models" write our code to the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Amplify, DataStore, Predicates } from "aws-amplify";
+import { Post, PostStatus } from "./models";
+
+// Use next two lines only if syncing with the cloud
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+async function onCreate() {
+  await DataStore.save(
+    new Post({
+      title: `New title ${Date.now()}`,
+      rating: Math.floor(Math.random() * (8 - 1) + 1),
+      status: PostStatus.ACTIVE,
+    })
+  );
+}
+
+async function onDeleteAll() {
+  await DataStore.delete(Post, Predicates.ALL);
+}
+
+async function onQuery() {
+  const posts = await DataStore.query(Post, (c) => c.rating.gt(4));
+
+  console.log(posts);
+}
 
 function App() {
+  useEffect(() => {
+    const subscription = DataStore.observe(Post).subscribe((msg) => {
+      console.log(msg.model, msg.opType, msg.element);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <input type="button" value="NEW" onClick={onCreate} />
+          <input type="button" value="DELETE ALL" onClick={onDeleteAll} />
+          <input type="button" value="QUERY rating > 4" onClick={onQuery} />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
